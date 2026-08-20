8d97a2b058b26af52a4db4ee75774570fe173c24
Tisha Dey <tishadey2025@outlook.com>
2026-08-20 10:50:53 +0600

    :memo: first commit

---
+++ problem-1.js
@@ -0,0 +1,28 @@
+function matchWinner(teamAGoals, teamBGoals) {
+  // Write your code here…
+  // console.log(teamAGoals,teamBGoals);
+  if(typeof teamAGoals !== "number" || typeof teamBGoals !== "number"){
+    return "invalid"
+  }
+  if(teamAGoals > teamBGoals){
+    return "Team A Won" ;
+  }else if(teamBGoals > teamAGoals){
+    return "Team B Won";
+  }
+  // return teamAGoals;
+  // return teamBGoals
+  return 'Draw';
+}
+
+// const win = 2,1;
+// const result = matchWinner(2,1);
+// console.log(result);
+
+// const win = matchWinner(1, 3);
+// console.log(win)
+
+// const win = matchWinner(2, 2);
+// console.log(win)
+
+const win = matchWinner("3", 2);
+console.log(win)
